c961d8826e7fb55f79358afae188f7f43e58f743
ts-miller <tylerstephen11@gmail.com>
2020-11-30 21:39:37 -0500

    commit tree ea0974d275224e59bb1a553d4c3512ebca6735ec

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
-// Your code here
+function mapToNegativize(array) {
+    let newArray = []
+    for(const num of array) {
+        newArray.push(num * -1)
+    }
+    return newArray
+}
+
+function mapToNoChange(array) {
+    return array
+}
+
+function mapToDouble(array) {
+    let newArray = []
+    for(const num of array) {
+        newArray.push(num * 2)
+    }
+    return newArray
+}
+
+function mapToSquare(array) {
+    let newArray = []
+    for(const num of array) {
+        newArray.push(num * num)
+    }
+    return newArray
+}
+
+function reduceToTotal(array, start = 0) {
+    let total = start
+    for (let i = 0; i < array.length; i++) {
+        total += array[i]
+    }
+    return total
+}
+
+function reduceToAllTrue(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === false) {
+            return false
+        }
+    }
+    return true
+}
+
+function reduceToAnyTrue(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === true) {
+            return true
+        }
+    }
+    return false
+}
